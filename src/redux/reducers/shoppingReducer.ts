4c0da867b4de793392e52658a08977cb9dddfd1f
gechjs <gizachew980@gmail.com>
2025-07-30 12:00:00 +0300

    feat: add initial sample data for categories and restaurants

--- src/redux/reducers/shoppingReducer.ts
+++ src/redux/reducers/shoppingReducer.ts
@@ -2,8 +2,27 @@ import { ShoppingState } from '../models';
 import { ShoppingAction } from '../actions/shoppingAction';
 
 const initialState: ShoppingState = {
-  availability: { categories: [], restaurants: [], foods: [] },
-  availableFoods: [],
+  availability: {
+    categories: [
+      { id: '1', title: 'Pizza', icon: 'https://dummyimage.com/100x100/000/fff&text=Pizza' },
+      { id: '2', title: 'Burgers', icon: 'https://dummyimage.com/100x100/000/fff&text=Burgers' },
+    ],
+    restaurants: [
+      {
+        _id: 'r1',
+        name: 'Express Pizza',
+        foodType: 'Italian',
+        address: 'Central Street 12',
+        phone: '9999999999',
+        images: ['https://dummyimage.com/400x200/000/fff&text=Express+Pizza'],
+        foods: [
+          { _id: 'f1', name: 'Margherita', description: 'Classic cheese pizza', category: 'Pizza', price: 250, readyTime: 20, images: ['https://dummyimage.com/400x200/000/fff&text=Margherita'], unit: 0 },
+        ],
+      },
+    ],
+    foods: [{ _id: 'f1', name: 'Margherita', description: 'Classic cheese pizza', category: 'Pizza', price: 250, readyTime: 20, images: ['https://dummyimage.com/400x200/000/fff&text=Margherita'], unit: 0 }],
+  },
+  availableFoods: [{ _id: 'f1', name: 'Margherita', description: 'Classic cheese pizza', category: 'Pizza', price: 250, readyTime: 20, images: ['https://dummyimage.com/400x200/000/fff&text=Margherita'], unit: 0 }],
 };
 
 export const ShoppingReducer = (state: ShoppingState = initialState, action: ShoppingAction): ShoppingState => {
